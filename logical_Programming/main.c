eea31362b43c3895193cbce5ed2f8aa66fcbf470
dileepthutta <thuttadileep@gmail.com>
2025-03-31 21:29:23 +0530

    first commit

---
+++ logical_Programming/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+        int a,b;
+        printf("Enter a value :");
+        scanf("%d",&a);
+        printf("Enter b value :");
+        scanf("%d",&b);
+        a>b? printf("%d is big",a):printf("%d is big",b);
+        return 0;
+   }
